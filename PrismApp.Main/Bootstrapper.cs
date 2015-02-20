94d798c7aaf316db52aaa0765cc39de0e03008dc
dana.nurse <dana.nurse@outlook.com>
2015-02-20 23:08:44 +0000

    Initial Commit

---
+++ PrismApp.Main/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Prism;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Prism.UnityExtensions;
+
+namespace PrismApp.Main
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            // Create the Shell window and show it
+            MainWindow shell = Container.Resolve<MainWindow>();
+            shell.Show();
+            return shell;
+        }
+    }
+}
